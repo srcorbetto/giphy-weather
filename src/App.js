2b411441ff699137d40aac628c55c3ef04d7ee0c
Mike Corbett <srcorbetto@gmail.com>
2018-09-16 16:51:12 -0500

    Updated API routes to call from the server

--- src/App.js
+++ src/App.js
@@ -4,28 +4,7 @@ import SearchBar from './components/SearchBar/SearchBar';
 
 import './App.css';
 
-let testVar;
-
 class App extends Component {
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => {
-        testVar = res.express;
-        console.log(testVar);
-        return testVar
-      })
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
 
   render() {
 
